5a3cd6385c77195da7aa1bca7a613ef01a0e706c
Talha86-ux <rjtalha555@gmail.com>
2024-09-08 20:06:53 +0500

    Add skills and experience section WIP

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import skills from '../../data/skills.json'
+import history from '../../data/history.json'
+import { getImageUrl } from '../../utils'
+
+export const Experience = ()=> {
+  return(
+    <section id='experience'>
+      <h2>Experience</h2>
+      <div>
+        <div>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id}>
+                <div>
+                  <img src={getImageUrl(skill.imageSrc)} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <ul>
+          {history.map((historyItem, id)=> {
+            return(
+              <li key={id}>
+                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
+                <div>
+                  <h3>{`${historyItem.role} , ${historyItem.organisation}`}</h3>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </section>
+  )
+}
