efb4052a548673fd987847949f866595e14f38a5
Talha86-ux <rjtalha555@gmail.com>
2024-09-14 19:14:04 +0500

    Setup basic projects section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Navbar } from './components/Navbar/Navbar'
 import { About } from './components/About/About'
 import Hero from './components/Hero/Hero'
 import { Experience } from './components/Experience/Experience'
+import { Projects } from './components/Projects/Projects'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Hero/>
       <About/>
       <Experience/>
+      <Projects/>
     </div>
   )
 }
