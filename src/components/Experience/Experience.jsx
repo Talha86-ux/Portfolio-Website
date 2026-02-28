282324e0ff2c304955cfcc57a78d47446863832a
Talha86-ux <rjtalha555@gmail.com>
2026-03-01 03:52:41 +0500

    Remove innecessary info

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import styles from './Experience.module.css'
 import skills from '../../data/skills.json'
-import history from '../../data/history.json'
 import { getImageUrl } from '../../utils'
 
 export const Experience = ()=> {
   return(
     <section className={styles.container} id='experience'>
-      <h2 className={styles.title}>Experience</h2>
+      <h2 className={styles.title}>Skills & Experience</h2>
       <div className={styles.content}>
         <div className={styles.skills}>
           {skills.map((skill, id) => {
@@ -21,26 +20,6 @@ export const Experience = ()=> {
             );
           })}
         </div>
-        <ul className={styles.history}>
-          {history.map((historyItem, id)=> {
-            return(
-              <li key={id} className={styles.historyItem}>
-                <div className={styles.historyLogo}>
-                  <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
-                </div>
-                <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role} , ${historyItem.organisation}`}</h3>
-                  <p style={{ fontSize: '15px' }}>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experience, id) => {
-                      return <li key={id}> {experience}</li>
-                    })}
-                  </ul>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
       </div>
     </section>
   )
